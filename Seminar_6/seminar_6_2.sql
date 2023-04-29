e0ee669283d0d736d8b29a63860d5944f282b4e7
Valet78 <valet78@mail.ru>
2023-04-30 01:06:53 +0300

    Решение ДЗ семинара 6 SQL коррекция

--- Seminar_6/seminar_6_2.sql
+++ Seminar_6/seminar_6_2.sql
@@ -1,4 +1,20 @@
 CREATE DATABASE IF NOT EXISTS db_less6;
 USE db_less6;
-
+DROP FUNCTION IF EXISTS even_out;
 # Задание 2.
+DELIMITER //
+CREATE FUNCTION even_out() RETURNS VARCHAR(50)
+	DETERMINISTIC
+BEGIN
+	DECLARE i INT DEFAULT 2;
+    DECLARE result VARCHAR(50) DEFAULT '';
+    
+	WHILE i < 12 DO
+		SET result = CONCAT_WS(' ', result, i);
+		SET i = i + 2;
+	END WHILE;
+
+	RETURN result;
+END //
+
+SELECT even_out() AS result;
